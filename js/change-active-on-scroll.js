d4acc6137de3901cc53e2b990e3125000d12d12e
Matias <novellomatias@yahoo.com.ar>
2016-04-06 14:57:37 -0300

    Se agregaron astros,florencia-vario,prevercred,chat, mujer.
    Se hicieron cambios en brindis y gamers.
    Se agrego idioma a el form de contacto.

---
+++ js/change-active-on-scroll.js
@@ -0,0 +1,24 @@
+$(function() {
+	var sections = $('div.section'), nav = $('nav'), nav_height = nav.outerHeight();
+	 
+	$(window).on('scroll', function () {
+	  var cur_pos = $(this).scrollTop();
+	 
+	  sections.each(function() {
+	    var top = $(this).offset().top - nav_height,
+	        bottom = top + $(this).outerHeight();
+	 
+	    if (cur_pos >= top && cur_pos <= bottom) {
+	      nav.find('a').removeClass('active');
+	      sections.removeClass('active');
+	 
+	      $(this).addClass('active');
+	      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+	    }
+	    if (cur_pos < 120) {
+	    	nav.find('a[href="#'+'port"]').removeClass('active');
+	    	nav.find('a[href="#'+'home"]').addClass('active');
+	    }
+	  });
+	});
+});
--- js/index.js
+++ js/index.js
@@ -8,16 +8,14 @@ var app = {
 
 		$(function() {
 			$(".lang-Changer-es-index").click(function(event) {
-				$('[data-translate]').jqTranslate('index', { forceLang : 'es',path:'js/langpack' }); return false;
-				$('#contactForm').formValidation('setLocale', 'es_ES');
-				$('#contactForm').formValidation.reCaptcha2.language('es');				
+				$('#contactForm').formValidation('setLocale', 'es_ES');				
+				$('[data-translate]').jqTranslate('index', { forceLang : 'es',path:'js/langpack' }); return false;							
 			});
 			$(".lang-Changer-en-index").click(function(event) {
+				$('#contactForm').formValidation('setLocale', 'en_US');				
 				$('[data-translate]').jqTranslate('index', { forceLang : 'en',path:'js/langpack' }); return false;
-				$('#contactForm').formValidation('setLocale', 'en_US');
-				$('#contactForm').formValidation.reCaptcha2.language('en');
 			});
-		});
+		});		
 
 		$(function() {
 			$(".scroll").click(function(event) {
@@ -82,7 +80,10 @@ var app = {
 					name: {
 						validators: {
 							notEmpty: {
-								message: 'El campo es obligatorio.'
+								message: {
+									en_US: 'The name is required.',
+									es_ES: 'El campo es obligatorio.'
+								}								 
 							}
 						}
 					},
@@ -90,46 +91,67 @@ var app = {
 					tel: {
 						validators: {
 							notEmpty: {
-								message: 'El campo es obligatorio.'
+								message: {
+									en_US: 'The phone number is required.',
+									es_ES: 'El campo es obligatorio.'
+								}
 							},
 							stringLength: {
 								min: 7,
 								max: 13,
-								message: 'El t&eacute;lefono debe tener entre 7 y 13 n&uacute;meros.'
+								message: {
+									en_US: 'The phone must be between 7 and 13 numbers.',
+									es_ES: 'El t&eacute;lefono debe tener entre 7 y 13 n&uacute;meros.'
+								}
 							},
 							regexp: {
 								regexp: /^[0-9\s\-()+\.]+$/,
-								message: 'Debe ingresar solo n&uacute;meros.'
+								message: {
+									en_US: 'The phone number can only contain the digits.',
+									es_ES: 'Debe ingresar solo n&uacute;meros.'
+								}
 							}
 						}
 					},				
 					 email: {
 						validators: {
 							notEmpty: {
-								message: 'El campo es obligatorio.'
+								message: {
+									en_US: 'The e-mail is required.',
+									es_ES: 'El campo es obligatorio.'
+								}
 							},
 							emailAddress: {
-								message: 'El email ingresado es incorrecto.'
+								message: {
+									en_US: 'The input is not a valid email address.',
+									es_ES: 'El email ingresado es incorrecto.'
+								}
 							}
 						}
 					},
 					message: {
 						validators: {
 							notEmpty: {
-								message: 'El campo es obligatorio.'
+								message: {
+									en_US: 'The message is required.',
+									es_ES: 'El campo es obligatorio.'
+								}
 							}						
 						}
 					}
 				},
 				addOns: {
 					reCaptcha2: {
-						element: 'captchaContainer',
-						language: 'es',
+						element: 'captchaContainer',						
 						theme: 'dark',
 						siteKey: '6LcV3BcTAAAAALOu96DdhK_7nGq62rbHzLzcfTR2',
 						timeout: 120,
-						message: 'El captcha ingresado no es valido.'
-					}
+						message: {
+									en_US: 'The input is not a valid captcha.',
+									es_ES: 'El captcha ingresado no es valido.'
+						}						
+					},
+					i18n:{}
 				}				
 			})
 			 .on('success.form.fv', function(e) {
@@ -166,10 +188,46 @@ var app = {
 					}
 				});				
 			});
-
 		});
-		
-		
+
+		$(function() {
+			// Variable para guardar el idioma
+			var lang;
+			 
+			var nav = navigator.userAgent.toLowerCase();
+			if(nav.indexOf("msie") != -1 || nav.indexOf("rv") != -1){ // msi for IE and rv for IE11+
+			    var req = new XMLHttpRequest();
+			    req.open('GET', 'resources/inspect-headers.py?filter_name=accept-language', false);
+			    req.send(null);
+			    var headers = req.getAllResponseHeaders().toLowerCase();
+			    var contentLanguage = headers.match( /^content-language\:(.*)$/gm );
+			    if(contentLanguage[0]) {
+					lang= contentLanguage[0].split(": ")[1];
+			    }
+			} else {		
+			    if (navigator.languages==undefined) {
+					if (navigator.language==undefined) {
+						// Internet Explorer Compatibility
+						lang= navigator.userLanguage.slice(0,2);
+					} else {
+						// Old navigator compatibility
+						lang= navigator.language.slice(0,2);
+					}
+			    } else { 
+					lang= navigator.languages[0].slice(0,2);				
+			    }
+			}
+			if (lang == "es") {
+				$('#contactForm').formValidation('setLocale', 'es_ES');				
+				$('[data-translate]').jqTranslate('index', { forceLang : 'es',path:'js/langpack' }); return false;					
+			}else if (lang == "en") {
+				$('#contactForm').formValidation('setLocale', 'en_US');				
+				$('[data-translate]').jqTranslate('index', { forceLang : 'en',path:'js/langpack' }); return false;
+			}else{
+				$('#contactForm').formValidation('setLocale', 'es_ES');				
+				$('[data-translate]').jqTranslate('index', { forceLang : 'es',path:'js/langpack' }); return false;	
+			};			
+		});						
 
 	},
 
@@ -184,7 +242,6 @@ var app = {
 	hideURLbar : function() {
 		window.scrollTo(0, 1);
 	}
-
 };
 
 app.initialize();
